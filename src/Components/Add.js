971c658aaa6f070c56284f94814c7808f25c3e1c
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-02-23 23:39:40 +0500

    Underconstruction

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -33,7 +33,7 @@ export default function Add() {
             <br />
 
             <div>
-                <img src="https://images.olx.com.pk/thumbnails/434332564-800x600.webp" className="sm" alt="..." />
+                <img src="https://images.olx.com.pk/thumbnails/434332564-800x600.webp" className="smimg" alt="..." />
             </div>
 
         </div >
