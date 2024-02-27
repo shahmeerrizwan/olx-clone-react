918b7d43630c9412f849ac31bdba7cf91182c79c
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-02-27 23:54:52 +0500

    Underconstruction

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Add from '../Components/Add'
 import Category from '../Components/Category'
 import EndAdd from '../Components/EndAdd'
-// import HomeCard from '../Components/HomeCard'
+import HomeCard from '../Components/HomeCard'
 import HomeCard1 from '../Components/HomeCard1'
+import HomeCard2 from '../Components/HomeCard2'
+import HomeCard3 from '../Components/HomeCard3'
+
 
 
 
@@ -15,11 +18,11 @@ export default function Home() {
             <Category />
             <br />
             <br />
-            {/* <HomeCard /> */}
-            <br />
-            <br />
-
+            <HomeCard />
             <HomeCard1 />
+
+            <HomeCard2 />
+            <HomeCard3 />
             <br />
 
             <br />
