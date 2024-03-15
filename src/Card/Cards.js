79079c47e12158671f6b48a7f140a60c384f6761
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-03-15 23:35:00 +0500

    Completed

--- src/Card/Cards.js
+++ src/Card/Cards.js
@@ -9,7 +9,8 @@ export default function index({ images, title, description, price, brand }) {
                 <div className="containerr ">
                     <div className="cardd">
                         <div className="card-headerr">
-                            <img src={images[0]} alt="404 Error" />
+
+                            <img src={!images[0] ? "https://res.cloudinary.com/teepublic/image/private/s--UNm73W6m--/t_Preview/b_rgb:191919,c_limit,f_auto,h_630,q_90,w_630/v1606803184/production/designs/16724220_0.jpg" : images[0]} alt="Img Not Found" />
                         </div>
                         <div className="card-bodyy">
                             <span className="tagg tag-teall">Brand : {brand}</span>
@@ -27,7 +28,7 @@ export default function index({ images, title, description, price, brand }) {
                     </div>
                     <div className="cardd">
                         <div className="card-headerr">
-                            <img src={images[1]} alt="404 Error" />
+                            <img src={!images[1] ? "https://res.cloudinary.com/teepublic/image/private/s--UNm73W6m--/t_Preview/b_rgb:191919,c_limit,f_auto,h_630,q_90,w_630/v1606803184/production/designs/16724220_0.jpg" : images[1]} alt="Img Not Found" />
                         </div>
                         <div className="card-bodyy">
                             <span className="tagg tag-teall">Brand : {brand}</span>
@@ -45,7 +46,7 @@ export default function index({ images, title, description, price, brand }) {
                     </div>
                     <div className="cardd">
                         <div className="card-headerr">
-                            <img src={images[2]} alt="404 Error" />
+                            <img src={!images[2] ? "https://res.cloudinary.com/teepublic/image/private/s--UNm73W6m--/t_Preview/b_rgb:191919,c_limit,f_auto,h_630,q_90,w_630/v1606803184/production/designs/16724220_0.jpg" : images[2]} alt="Img Not Found" />
                         </div>
                         <div className="card-bodyy">
                             <span className="tagg tag-teall">Brand : {brand}</span>
