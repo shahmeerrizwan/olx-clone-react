3ea61ac0df49567c5eabd8a03058884dc07e17ef
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-03-15 20:19:34 +0500

    Completed

--- src/Pages/Delivery.js
+++ src/Pages/Delivery.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
+
+
 const DeliveryPage = () => {
     const handleFormSubmit = (event) => {
         event.preventDefault();
