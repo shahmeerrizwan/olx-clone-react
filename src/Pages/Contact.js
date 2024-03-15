ba59aee55eae78eac975665cce7d84dd0f91834d
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-03-15 20:19:01 +0500

    Completed

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,26 +4,31 @@ import '../Components/Contact.css';
 
 const Contact = () => {
     return (
-        <div className="contact-container">
-            <div className="contact-content">
-                <h1>Contact Us</h1>
-                <p>We'd love to hear from you!</p>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="name">Your Name</label>
-                        <input type="text" className="form-control" id="name" required />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Your Email</label>
-                        <input type="email" className="form-control" id="email" required />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="message">Message</label>
-                        <textarea className="form-control" id="message" rows="5" required></textarea>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Send Message</button>
-                </form>
+        <div>
+
+
+            <div className="contact-container">
+                <div className="contact-content">
+                    <h1>Contact Us</h1>
+                    <p>We'd love to hear from you!</p>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="name">Your Name</label>
+                            <input type="text" className="form-control" id="name" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Your Email</label>
+                            <input type="email" className="form-control" id="email" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="message">Message</label>
+                            <textarea className="form-control" id="message" rows="5" required></textarea>
+                        </div>
+                        <button type="submit" className="btn ">Send Message</button>
+                    </form>
+                </div>
             </div>
+
         </div>
     );
 }
